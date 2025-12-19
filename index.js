69c8ad9410c5081a6ac713c2d8fa95a09029ccc4
ManhDung08 <dung.tmepr@gmail.com>
2025-12-19 17:42:49 +0700

    Add api system info

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 app.get("/", async (req, res) => {
   res.status(200).send("Xin chào bạn");
 });
+const os = require("os");
+
+app.get("/system", async (req, res) => {
+  const systemInfo = {
+    platform: os.platform(),
+    cpuArch: os.arch(),
+    totalMemory: os.totalmem(),
+    freeMemory: os.freemem(),
+    uptime: os.uptime(),
+  };
+  res.status(200).json(systemInfo);
+});
 // Start server
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
